7b7c5e20d19249674c82225c5e6f6e7cb2b92cd8
irinaaZ <34382687+irinaaZ@users.noreply.github.com>
2021-02-21 22:30:28 +0200

    sql files were added

---
+++ com.banking.app/src/main/resources/sql/crudOperations.sql
@@ -0,0 +1,33 @@
+INSERT INTO bank.public.banks (name, phone, type, able_to_buy_currency_online, number_of_branches, address, currencies)
+VALUES ('Monobank', '0800205205', 'Global', true, 1, 'Kiev, 9 Institutska Street', 'USD; EUR; UAH');
+
+UPDATE bank.public.banks
+SET currencies = 'USD; EUR; UAH; TRY'
+WHERE id = 1;
+
+SELECT *
+FROM bank.public.banks
+WHERE address LIKE '%Kiev%';
+
+DELETE
+FROM bank.public.banks
+WHERE name = 'Monobank';
+
+INSERT INTO bank.public.currencies (bank_id, name, short_name, purchase_rate, selling_rate)
+VALUES (1, 'US Dollar', 'USD', 28.99, 29.55);
+
+UPDATE bank.public.currencies
+SET selling_rate = 29.76
+WHERE name = 'US Dollar';
+
+SELECT short_name
+FROM bank.public.currencies
+WHERE bank_id = 1;
+
+DELETE
+FROM bank.public.currencies
+WHERE short_name = 'USD';
+
+SELECT *
+FROM bank.public.banks b
+INNER JOIN bank.public.currencies c on b.id = c.bank_id;
