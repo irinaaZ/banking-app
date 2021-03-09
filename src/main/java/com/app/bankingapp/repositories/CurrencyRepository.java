a04b31f92aef643e893fb9ff864f6c276cbcad5c
irinaaZ <34382687+irinaaZ@users.noreply.github.com>
2021-03-09 20:11:45 +0200

    optional implementation (#5)

--- src/main/java/com/app/bankingapp/repositories/CurrencyRepository.java
+++ src/main/java/com/app/bankingapp/repositories/CurrencyRepository.java
@@ -3,11 +3,14 @@ package com.app.bankingapp.repositories;
 import com.app.bankingapp.domain.Currency;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface CurrencyRepository {
-    Currency create(Currency currency);
+    Optional<Currency> create(Currency currency);
     void delete(Long id);
-    Currency update(Currency currency);
-    Currency get(Long id);
+    Optional<Currency> update(Currency currency);
+    Optional<Currency> get(Long id);
     List<Currency> getAll();
+    Set<String> getCurrenciesWithHighPurchaseRate();
 }
--- src/main/java/com/app/bankingapp/repositories/impl/CurrencyRepositoryImpl.java
+++ src/main/java/com/app/bankingapp/repositories/impl/CurrencyRepositoryImpl.java
@@ -2,14 +2,19 @@ package com.app.bankingapp.repositories.impl;
 
 import com.app.bankingapp.domain.Currency;
 import com.app.bankingapp.repositories.CurrencyRepository;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public class CurrencyRepositoryImpl implements CurrencyRepository {
@@ -23,7 +28,7 @@ public class CurrencyRepositoryImpl implements CurrencyRepository {
     }
 
     @Override
-    public Currency create(Currency currency) {
+    public Optional<Currency> create(Currency currency) {
         GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(con -> {
             PreparedStatement preparedStatement =
@@ -45,7 +50,7 @@ public class CurrencyRepositoryImpl implements CurrencyRepository {
     }
 
     @Override
-    public Currency update(Currency currency) {
+    public Optional<Currency> update(Currency currency) {
         jdbcTemplate.update(con -> {
             PreparedStatement preparedStatement =
                     con.prepareStatement("UPDATE bank.public.currencies SET bank_id = ?, name = ?, short_name = ?, " +
@@ -62,13 +67,27 @@ public class CurrencyRepositoryImpl implements CurrencyRepository {
     }
 
     @Override
-    public Currency get(Long id) {
-        return jdbcTemplate.queryForObject("SELECT * FROM bank.public.currencies WHERE id = ?",
-                currencyRowMapper, id);
+    public Optional<Currency> get(Long id) {
+        try {
+            return Optional
+                    .ofNullable(jdbcTemplate.queryForObject("SELECT * FROM bank.public.currencies WHERE id = ?",
+                            currencyRowMapper, id));
+        } catch (DataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Currency> getAll() {
         return jdbcTemplate.query("SELECT * FROM bank.public.currencies", currencyRowMapper);
+    }
+
+    @Override
+    public Set<String> getCurrenciesWithHighPurchaseRate() {
+        return getAll()
+                .stream()
+                .filter(currency -> currency.getPurchaseRate().compareTo(new BigDecimal("26.00")) > 0)
+                .map(Currency::getName)
+                .collect(Collectors.toSet());
     }
 }
