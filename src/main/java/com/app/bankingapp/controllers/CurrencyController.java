8a355986a8073638b3a9c8339d3db17ba997ca1a
irinaaZ <34382687+irinaaZ@users.noreply.github.com>
2021-03-16 22:02:38 +0200

    fix url in CurrencyController

--- src/main/java/com/app/bankingapp/controllers/CurrencyController.java
+++ src/main/java/com/app/bankingapp/controllers/CurrencyController.java
@@ -10,38 +10,43 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(value = "/v1/banks/currencies")
+@RequestMapping(value = "/v1/banks")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/currencies/{id}")
     public CurrencyDto get(@PathVariable Long id) {
         return currencyService.get(id);
     }
 
-    @GetMapping
+    @GetMapping(value = "/currencies")
     public List<CurrencyDto> getAll() {
         return currencyService.getAll();
     }
 
-    @PostMapping
+    @PostMapping(value = "/{bankId}/currencies")
     @ResponseStatus(HttpStatus.CREATED)
-    public CurrencyDto create(@RequestBody CurrencyDto currencyDto) {
+    public CurrencyDto create(
+            @PathVariable Long bankId,
+            @RequestBody CurrencyDto currencyDto) {
+        currencyDto.setBankId(bankId);
         return currencyService.create(currencyDto);
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/currencies/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         currencyService.delete(id);
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/{bankId}/currencies/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CurrencyDto update(
+            @PathVariable Long bankId,
             @PathVariable Long id,
             @RequestBody CurrencyDto currencyDto) {
+        currencyDto.setBankId(bankId);
         currencyDto.setId(id);
         return currencyService.update(currencyDto);
     }
